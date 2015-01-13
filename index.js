1c5cdbead2bfb1f26c3de79d5731b175358eaa96
chrtze <chrtze@gmail.com>
2015-01-13 16:23:43 +0100

    remove vars

--- index.js
+++ index.js
@@ -7,11 +7,9 @@ var dsv = require('dsv');
 module.exports = function(text) {
 
   var query = loaderUtils.parseQuery(this.query),
-      delimiter = query.delimiter || ',';
-  
-  var parser = dsv(delimiter);
-
-  var res = parser.parse(text);
+      delimiter = query.delimiter || ',',
+      parser = dsv(delimiter),
+      res = parser.parse(text);
 
   return "module.exports = " + JSON.stringify(res, undefined, "\t");
 }
