1e0cdaeddf800f7e50954fc406715a8586997eeb
g-div <g-div@anche.no>
2018-10-12 15:35:59 +0200

    refactor(loader): reindent lines

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-/*  dsv-loader: a dsv loader for webpack
-    built using dsv by Mike Bostock */
+/*
+  dsv-loader: a dsv loader for webpack
+  built using dsv by Mike Bostock
+*/
 
 const { getOptions } = require('loader-utils');
 const { dsvFormat } = require('d3-dsv');
@@ -8,10 +10,10 @@ module.exports = function(text) {
   this.cacheable();
 
   const options = getOptions(this) || {},
-      delimiter = options.delimiter || ',',
-      dsv = dsvFormat(delimiter),
-      rows = options.rows,
-      res = rows ? dsv.parseRows(text) : dsv.parse(text);
+    delimiter = options.delimiter || ',',
+    dsv = dsvFormat(delimiter),
+    rows = options.rows,
+    res = rows ? dsv.parseRows(text) : dsv.parse(text);
 
   return `const res = ${JSON.stringify(res)};
     res.columns = ${JSON.stringify(res.columns)};
